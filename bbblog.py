49bceb70e14be45dfbc2711876f3865f71f1d034
Baptiste Mispelon <bmispelon@gmail.com>
2024-05-09 22:05:04 +0200

    Added header image randomization to articles

--- bbblog.py
+++ bbblog.py
@@ -7,6 +7,7 @@ from dataclasses import dataclass
 from datetime import date, datetime
 from functools import partial
 from pathlib import Path
+import random
 import sys
 import textwrap
 
@@ -28,6 +29,23 @@ def render_template(name: str, **context) -> str:
 def _xpath(tree, xpath:str):
     found, = tree.xpath(xpath)
     return found
+
+
+def _prettyprint_html(tree):
+    return html.tostring(
+        tree,
+        pretty_print=True,
+        encoding="unicode",
+        doctype="<!doctype html>"
+    )
+
+
+def get_random_header_variant():
+    """
+    Return a random variant class for the <header> (see styles.css)
+    """
+    variants = [f"variant{i}" for i in range(1, 8)]
+    return random.choice(variants)
 
 
 @dataclass
@@ -69,7 +87,18 @@ def rewrite_dates(source: str, old_date: date, new_date: date) -> str:
         node.attrib["datetime"] = new_date.isoformat()
         node.text = new_date.strftime("%B %-dth")
 
-    return html.tostring(parsed, pretty_print=True, encoding="unicode")
+    return _prettyprint_html(parsed)
+
+
+def rewrite_header_class(source: str, new_header_class) -> str:
+    """
+    Replace the class of the <header> in the given source and return the new
+    source.
+    """
+    parsed = html.fromstring(source)
+    header = _xpath(parsed, "//body/header")
+    header.attrib["class"] = new_header_class
+    return _prettyprint_html(parsed)
 
 
 app = typer.Typer()
@@ -96,6 +125,7 @@ def mkarticle(title: list[str]) -> Path:
     context = {
         "title": " ".join(title),
         "pubdate": TODAY,
+        "header_variant": get_random_header_variant(),
     }
 
     articlepath = BLOG_DIR / "articles" / str(TODAY.year) / f"{TODAY.isoformat()}-{slug}.html"
@@ -103,7 +133,6 @@ def mkarticle(title: list[str]) -> Path:
     articlepath.write_text(render_template("article.html", **context))
     print(f"Created a new article at {articlepath.relative_to(Path.cwd())}")
     return articlepath
-
 
 
 @app.command()
@@ -127,7 +156,21 @@ def changedate(filepath: Path, newdate: datetime = datetime.now()) -> Path:
     newpath.write_text(newcontent)
 
     print(f"Renamed and updated {filepath} -> {newpath}")
+    print("Make sure you run `just prettier` to reformat it")
     return newpath
+
+
+@app.command()
+def randomizeheader(filepaths: list[Path]):
+    """
+    Change the header variant class used in the given file(s)
+    """
+    for filepath in filepaths:
+        variant = get_random_header_variant()
+        old_source = filepath.read_text()
+        new_source = rewrite_header_class(old_source, f"container {variant}")
+        filepath.write_text(new_source)
+        print(f"File {filepath} got new variant {variant}")
 
 
 if __name__ == '__main__':
--- justfile
+++ justfile
@@ -10,6 +10,23 @@ serve port='8888':
 prettier:
     git ls-files -x vendors blog/ | grep -v /vendors/ | grep -E '\.(html|css|js)$' | xargs prettier -w
 
+# Generate CSS rules for header variants
+headervariants:
+    #!/usr/bin/env bash
+    i=1
+    for x in `ls blog/static/css/images/headers/*.jpeg`
+    do
+        xx=$(basename $x)
+        xxx=${xx%.*}
+
+        echo "header.variant$i {"
+        echo "  --header-image-jpeg: url(\"./images/headers/$xxx.jpeg\");"
+        echo "  --header-image-avif: url(\"./images/headers/$xxx.avif\");"
+        echo "  --header-image-webp: url(\"./images/headers/$xxx.webp\");"
+        echo "}"
+        ((i++))
+    done
+
 # call the blog engine script bbblog.py
 blog *a:
     python bbblog.py {{a}}
